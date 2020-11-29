0962ce7f0b83799c2be0e1c21015be6d4305ce15
rainbowunicornsj <74851527+rainbowunicornsj@users.noreply.github.com>
2020-11-29 16:59:50 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     Joanna.setPosition(60, 100)
 })
@@ -8,7 +13,13 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     Joanna.setPosition(130, 100)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-	
+    otherSprite.destroy(effects.confetti, 500)
+    info.changeScoreBy(1)
+    count += 1
+    if (count == 10) {
+        info.changeLifeBy(1)
+        count = 0
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     Joanna.setPosition(100, 100)
@@ -16,11 +27,16 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 info.onLifeZero(function () {
     game.over(false, effects.splatter)
 })
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    sprite.destroy()
+    info.changeLifeBy(-1)
+})
 let right: Sprite = null
 let down: Sprite = null
 let up: Sprite = null
 let left: Sprite = null
 let lane = 0
+let count = 0
 let Joanna: Sprite = null
 tiles.setTilemap(tiles.createTilemap(hex`0a0008000202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020201010101010101010101`, img`
     . . . . . . . . . . 
@@ -58,7 +74,7 @@ info.setLife(5)
 game.onUpdateInterval(2000, function () {
     speed += 1
 })
-game.onUpdateInterval(500, function () {
+game.onUpdateInterval(450, function () {
     lane = randint(1, 4)
     if (lane == 1) {
         left = sprites.create(img`
@@ -66,19 +82,19 @@ game.onUpdateInterval(500, function () {
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a 1 1 a a a a a a a a 
-            a a a a a 1 1 a a a a a a a a a 
+            a a a a a 1 1 1 a a a a a a a a 
             a a a a 1 1 1 1 1 1 1 1 1 a a a 
             a a a 1 1 1 1 1 1 1 1 1 1 a a a 
             a a a 1 1 1 1 1 1 1 1 1 1 a a a 
             a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a a 1 1 a a a a a a a a a 
+            a a a a a 1 1 1 a a a a a a a a 
             a a a a a a 1 1 a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
-            `, SpriteKind.Player)
+            `, SpriteKind.Projectile)
         left.setVelocity(0, speed)
         left.setPosition(30, 8)
     } else if (lane == 2) {
@@ -86,20 +102,20 @@ game.onUpdateInterval(500, function () {
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
-            a a a a a a 1 1 a a a a a a a a 
-            a a a a a 1 1 a a a a a a a a a 
-            a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a 1 1 1 1 1 1 1 1 1 1 a a a 
-            a a a 1 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a a 1 1 a a a a a a a a a 
-            a a a a a a 1 1 a a a a a a a a 
+            a a a a a a a a 1 1 a a a a a a 
+            a a a a a a a 1 1 1 1 a a a a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
+            a a a a a 1 1 1 1 1 1 1 1 a a a 
+            a a a a 1 1 1 1 1 1 1 1 1 1 a a 
+            a a a a 1 1 1 1 1 1 1 1 1 1 a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
+            a a a a a a 1 1 1 1 1 1 a a a a 
             a a a a a a a a a a a a a a a a 
-            a a a a a a a a a a a a a a a a 
-            a a a a a a a a a a a a a a a a 
-            a a a a a a a a a a a a a a a a 
-            a a a a a a a a a a a a a a a a 
-            `, SpriteKind.Player)
+            `, SpriteKind.Projectile)
         up.setVelocity(0, speed)
         up.setPosition(60, 8)
     } else if (lane == 3) {
@@ -107,20 +123,20 @@ game.onUpdateInterval(500, function () {
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
-            a a a a a a 1 1 a a a a a a a a 
-            a a a a a 1 1 a a a a a a a a a 
-            a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a 1 1 1 1 1 1 1 1 1 1 a a a 
-            a a a 1 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a a 1 1 a a a a a a a a a 
-            a a a a a a 1 1 a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
+            a a a a a a 1 1 1 1 a a a a a a 
+            a a a a a a 1 1 1 1 a a a a a a 
+            a a a a a a 1 1 1 1 a a a a a a 
+            a a a a a a 1 1 1 1 a a a a a a 
+            a a a a 1 1 1 1 1 1 1 1 a a a a 
+            a a a a 1 1 1 1 1 1 1 1 a a a a 
+            a a a a a 1 1 1 1 1 1 a a a a a 
+            a a a a a a 1 1 1 1 a a a a a a 
+            a a a a a a a 1 1 a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
-            a a a a a a a a a a a a a a a a 
-            `, SpriteKind.Player)
+            `, SpriteKind.Projectile)
         down.setVelocity(0, speed)
         down.setPosition(100, 8)
     } else {
@@ -128,20 +144,20 @@ game.onUpdateInterval(500, function () {
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
-            a a a a a a 1 1 a a a a a a a a 
-            a a a a a 1 1 a a a a a a a a a 
-            a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a 1 1 1 1 1 1 1 1 1 1 a a a 
-            a a a 1 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a 1 1 1 1 1 1 1 1 1 a a a 
-            a a a a a 1 1 a a a a a a a a a 
-            a a a a a a 1 1 a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a 1 a a a a a 
+            a a a a a a a a a a 1 1 a a a a 
+            a a a a a 1 1 1 1 1 1 1 1 a a a 
+            a a a a a 1 1 1 1 1 1 1 1 1 a a 
+            a a a a a 1 1 1 1 1 1 1 1 1 a a 
+            a a a a a 1 1 1 1 1 1 1 1 a a a 
+            a a a a a a a a a a 1 1 a a a a 
+            a a a a a a a a a a 1 a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
             a a a a a a a a a a a a a a a a 
-            a a a a a a a a a a a a a a a a 
-            `, SpriteKind.Player)
+            `, SpriteKind.Projectile)
         right.setVelocity(0, speed)
         right.setPosition(130, 8)
     }
